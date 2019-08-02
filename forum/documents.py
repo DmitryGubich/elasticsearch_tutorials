4964e0cb99038cc04b9891affd8222a6b4288342
Dmitry Gubich <gubich97@gmail.com>
2019-08-02 17:42:03 +0300

    change document

--- forum/documents.py
+++ forum/documents.py
@@ -1,20 +1,32 @@
-from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 
-from forum.models import Category
+from forum.models import Category, Post
 
 
 @registry.register_document
 class CategoryDocument(Document):
+    posts = fields.NestedField(
+        properties={
+            'author_set': fields.NestedField(
+                properties={
+                    'car': fields.NestedField(
+                        properties={
+                            'brand': fields.StringField()
+                        }
+                    )
+                }
+            )
+        }
+    )
+
     class Index:
         name = 'categories'
-        settings = {'number_of_shards': 1,
-                    'number_of_replicas': 0}
 
     class Django:
         model = Category
 
-        fields = [
-            'name',
-            'slug'
-        ]
+    def get_instances_from_related(self, related_instance):
+        """If related_models is set, define how to retrieve the Book instance(s) from the related model."""
+        if isinstance(related_instance, Post):
+            return related_instance.post_set.all()
